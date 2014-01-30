77fda92097d18a9a3b33e56fc9ff0d25c341e553
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-01-30 09:36:16 -0500

    Added some constants.

--- src/comp361/shared/Constants.java
+++ src/comp361/shared/Constants.java
@@ -7,4 +7,12 @@ public class Constants {
 	public static final int PORT = 5000;
 	public static final String DATA_PATH = "./data/";
 	public static final String SERVER_DATA_PATH = DATA_PATH + "server/";
+	
+	// Screen size
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
+	
+	// Tile sizes
+	public static final int TILE_WIDTH = 24;
+	public static final int TILE_HEIGHT = 24;
 }
