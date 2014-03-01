cbace241a79d98586f6220c04e1cdc8e9c62d9a3
jokeofweek <dominiccharleyroy@gmail.com>
2014-03-01 11:50:40 -0500

    Auto-join a game when you create a game.

--- src/comp361/client/network/handlers/GameDescriptorCreatedPacketHandler.java
+++ src/comp361/client/network/handlers/GameDescriptorCreatedPacketHandler.java
@@ -1,15 +1,22 @@
 package comp361.client.network.handlers;
 
 import comp361.client.GameClient;
+import comp361.shared.packets.client.JoinGamePacket;
 import comp361.shared.packets.server.GameDescriptorCreatedPacket;
 
 public class GameDescriptorCreatedPacketHandler implements
 		ClientPacketHandler<GameDescriptorCreatedPacket> {
 
 	@Override
-	public void handle(GameClient gameClient, GameDescriptorCreatedPacket object) {
-		gameClient.getGameDescriptorManager().addDescriptor(object.descriptor);
-		// TODO: Actually join the game if you created it.
+	public void handle(GameClient gameClient, GameDescriptorCreatedPacket packet) {
+		gameClient.getGameDescriptorManager().addDescriptor(packet.descriptor);
+
+		// If you created the game, then we auto-join.
+		if (packet.isCreator) {
+			JoinGamePacket joinPacket = new JoinGamePacket();
+			joinPacket.id = packet.descriptor.getId();
+			gameClient.getClient().sendTCP(joinPacket);
+		}
 	}
 
 }
