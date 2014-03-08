9c70d084b29ea61f7f2f3f57955a6ff447357fde
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-03-08 11:27:39 -0500

    Reverted to the old version of Radar

--- src/comp361/shared/data/radar/Radar.java
+++ src/comp361/shared/data/radar/Radar.java
@@ -1,24 +1,33 @@
 package comp361.shared.data.radar;
 
-import java.awt.Point;
-import java.util.HashSet;
-import java.util.Set;
+/**
+ * All different types of radar will extend this class.
+ */
+public abstract class Radar {
 
-import comp361.shared.data.Ship;
+	private int width;
+	private int height;
 
-public class Radar {
-
-	public Radar()
-	{
-		
+	public Radar(int width, int height) {
+		this.width = width;
+		this.height = height;
 	}
 	
-	public Set<Point> getVisiblePoints(Ship s)
-	{
-		Set<Point> points = new HashSet<Point>();
-		
-		//TODO: complete implementation
-		
-		return points;
+	public int getWidth() {
+		return width;
 	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	/**
+	 * This function differs on the type of radar, and will be used to test whether a given radar
+	 * can see a position.
+	 * @param destinationX The X position that we are trying to see.
+	 * @param destinationY The Y position that we are trying to see.
+	 * @return True if the destination is in the radar's range.
+	 */
+	public abstract boolean inRange(int destinationX, int destinationY);
+	
 }
