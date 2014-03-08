93e3ea58295c8af000bd2c8f35a36e705264faf0
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-03-08 15:25:02 -0500

    Implemented firing torpedoes

--- src/comp361/shared/data/Direction.java
+++ src/comp361/shared/data/Direction.java
@@ -3,12 +3,31 @@ package comp361.shared.data;
 public enum Direction {
 	LEFT, UP, RIGHT, DOWN;
 	
+	private double angle;
+	
 	/**
 	 * @param d a direction
-	 * @return returns the opposite direction of d
+	 * @return the opposite direction of d
 	 */
 	public Direction opposite()
 	{
 		return values()[(this.ordinal()+2)%4];
+	}
+	
+	/**
+	 * @return the angle of the direction in radian
+	 */
+	public double angle()
+	{
+		return Math.PI-this.ordinal()*(Math.PI/2);		
+	}
+	
+	/**
+	 * @param d a direction
+	 * @return the angle between this and d
+	 */
+	public double angleBetween(Direction d)
+	{
+		return d.angle() - this.angle;
 	}
 }
