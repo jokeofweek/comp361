c6d2338095156e8cd5f6368d60d03a25f45f194a
Alex Coco <alex.coco@shopify.com>
2014-03-10 15:50:44 -0400

    Use cross platform laf

--- src/comp361/client/ClientApplication.java
+++ src/comp361/client/ClientApplication.java
@@ -1,12 +1,20 @@
 package comp361.client;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import comp361.client.ui.StartWindow;
 
 public class ClientApplication {
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+			// Could not load cross-platform look and feel
+		}
+		
 		// Show the start window!
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
