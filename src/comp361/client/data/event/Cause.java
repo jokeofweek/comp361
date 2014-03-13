5bdcbafc474b4bb31046a776808895b08560246a
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-03-13 17:44:20 -0400

    Fixed Cause and Effect toString

--- src/comp361/client/data/event/Cause.java
+++ src/comp361/client/data/event/Cause.java
@@ -1,7 +1,16 @@
 package comp361.client.data.event;
 
 public enum Cause {
-	CANNON,
-	TORPEDO,
-	MINE
+	CANNON("Cannon"),
+	TORPEDO("Torpedo"),
+	MINE("Mine");
+	
+	private String text;
+	private Cause(String text) {
+		this.text = text;
+	}
+	@Override
+	public String toString() {
+		return text;
+	}
 }
