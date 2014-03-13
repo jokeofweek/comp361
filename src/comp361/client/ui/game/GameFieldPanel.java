ddb3533ac8f3df0a2d216b796ca3221f3bc2c18b
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-03-13 15:20:33 -0400

    Fixed import conflict on pull.

--- src/comp361/client/ui/game/GameFieldPanel.java
+++ src/comp361/client/ui/game/GameFieldPanel.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Set;
-import java.util.TimerTask;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
