46761c826f7afcfa5c9018cef27bfc30ae73e4a0
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-03-14 10:32:38 -0400

    Cleaned up error messages.

--- src/comp361/shared/packets/server/LoginError.java
+++ src/comp361/shared/packets/server/LoginError.java
@@ -1,8 +1,16 @@
 package comp361.shared.packets.server;
 
 public enum LoginError {
-	NO_SUCH_ACCOUNT,
-	INVALID_CREDENTIALS,
-	ACCOUNT_IN_USE,
-	LOAD_ERROR
+	NO_SUCH_ACCOUNT("No account exists with that name."),
+	INVALID_CREDENTIALS("Those credentials were invalid. Please try again."),
+	ACCOUNT_IN_USE("Someone is already using that account. Please try again."),
+	LOAD_ERROR("An error occurred loading your account. Please try again later.");
+	private String text;
+	private LoginError(String text) {
+		this.text = text;
+	}
+	@Override
+	public String toString() {
+		return text;
+	}
 }
