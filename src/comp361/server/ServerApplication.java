f29367eea2beebd75c7ab9b195918f0c7860bd1f
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-03-27 11:49:18 -0400

    Upgraded Kryonet version.

--- src/comp361/server/ServerApplication.java
+++ src/comp361/server/ServerApplication.java
@@ -1,5 +1,8 @@
 package comp361.server;
 
+import com.esotericsoftware.minlog.Log;
+import com.esotericsoftware.minlog.Log.Logger;
+
 import comp361.server.data.store.PropertyAccountDataStore;
 import comp361.server.logger.StandardLogger;
 import comp361.shared.Constants;
@@ -7,6 +10,7 @@ import comp361.shared.Constants;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		Log.set(Log.LEVEL_DEBUG);
 		new GameServer(new StandardLogger(), Constants.PORT, new PropertyAccountDataStore());
 	}
 
